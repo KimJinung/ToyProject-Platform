99481bdbbbd10931c74943e53b920686b715fd7d
KimJinung <KimJinung@outlook.com>
2023-05-01 13:35:51 +0900

    Init spring boot project

---
+++ src/test/java/kimjinung/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package kimjinung.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
