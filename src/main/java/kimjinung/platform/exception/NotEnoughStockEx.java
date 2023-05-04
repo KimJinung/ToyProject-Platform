7dbeb907cfc4766fa1e9a9841e6823409e02316a
KimJinung <KimJinung@outlook.com>
2023-05-04 20:58:29 +0900

    Remove not use ex constructors

--- src/main/java/kimjinung/platform/exception/NotEnoughStockEx.java
+++ src/main/java/kimjinung/platform/exception/NotEnoughStockEx.java
@@ -1,19 +1,8 @@
 package kimjinung.platform.exception;
 
 public class NotEnoughStockEx extends RuntimeException{
-    public NotEnoughStockEx() {
-    }
 
     public NotEnoughStockEx(String message) {
         super(message);
     }
-
-    public NotEnoughStockEx(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NotEnoughStockEx(Throwable cause) {
-        super(cause);
-    }
-
 }
