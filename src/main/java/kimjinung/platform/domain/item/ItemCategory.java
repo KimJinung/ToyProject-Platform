9e5697a696269868862f3b0fd49706a61ef94fd2
KimJinung <KimJinung@outlook.com>
2023-05-19 18:00:29 +0900

    Redesign project
    - Add LAZY fetch at OneToOne relation
    - Refactor Item domain constructor

---
+++ src/main/java/kimjinung/platform/domain/item/ItemCategory.java
@@ -0,0 +1,35 @@
+package kimjinung.platform.domain.item;
+
+import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+@Entity
+public class ItemCategory {
+    @Id
+    @GeneratedValue(generator = "uuidGenerator")
+    @GenericGenerator(name = "uuidGenerator", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "item_category_id")
+    private UUID id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public ItemCategory() {
+    }
+
+    public ItemCategory(Item item, Category category) {
+        this.item = item;
+        this.category = category;
+    }
+}
